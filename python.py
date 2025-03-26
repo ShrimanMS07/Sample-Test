e9e10d4011b24b0a5a7a8a4da647e787bb02d1a3
ShrimanMS07 <shrimanms18@gmail.com>
2025-03-26 09:25:15 +0530

    Message(Python is moved to Git)

--- python.py
+++ python.py
@@ -1,1 +1,14 @@
-print("Hello")
+def calculate_sum(integers):
+    total = 0
+    for num in integers:
+        if num % 2 == 0:  
+            total += int(str(abs(num))[0]) * num  
+        else:  
+            if abs(num) < 10:  
+                total += num * num
+            else:
+                total += (abs(num) % 10) * num  
+    return total
+
+numbers = list(map(int, input("Enter integers separated by commas: ").split(',')))
+print("Result:", calculate_sum(numbers))
